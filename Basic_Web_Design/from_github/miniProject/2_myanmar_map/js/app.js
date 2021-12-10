3d3f5a84a4c038697d3552a71b82d85bd4b00948
s1thu <s1thu.w1n.5601@gmail.com>
2021-12-10 23:35:01 +0630

    1st Commit

---
+++ Basic_Web_Design/from_github/miniProject/2_myanmar_map/js/app.js
@@ -0,0 +1,8 @@
+let currentPlace = document.querySelector('.current-place');
+let allPath = document.querySelectorAll(".map path");
+
+allPath.forEach(x => {
+    x.addEventListener("mouseover",_=>{
+        currentPlace.innerText = x.getAttribute('title');
+    });
+})
